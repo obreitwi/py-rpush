ca3a9133b905ba4abe6725858b24e6c982346098
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2020-02-06 11:12:57 +0100

    v0.3.3: Ran black on sourcecode

--- rpush.py
+++ rpush.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # encoding: utf-8
 # Copyright (c) 2013-2016 Oliver Breitwieser
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -55,7 +55,7 @@ Options:
 
 from __future__ import print_function
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 import os
 import os.path as osp
@@ -68,10 +68,13 @@ import string
 import itertools as it
 
 import logging
+
 logging.basicConfig(
-        #  format='%(asctime)s | %(funcName)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S',
-        format='%(asctime)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S',
-        level = logging.INFO)
+    #  format='%(asctime)s | %(funcName)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S',
+    format="%(asctime)s: %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+    level=logging.INFO,
+)
 
 if sys.version_info < (3, 0):
     import ConfigParser as configparser
@@ -88,9 +91,11 @@ else:
 
 from docopt import docopt
 
+
 def random_string(
-        size=24,
-        chars=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+    size=24,
+    chars=string.ascii_uppercase + string.ascii_lowercase + string.digits,
+):
     return "".join([random.choice(chars) for i in range(size)])
 
 
@@ -100,18 +105,18 @@ class RPushHandler(object):
         self.extra_handling()
 
         cfg = configparser.ConfigParser()
-        cfg.read( os.path.expanduser(self.args["--config"]))
+        cfg.read(os.path.expanduser(self.args["--config"]))
 
         self.ssh_args = "ssh -x".split()
 
         section = "RPush"
         if args["-H"] is None:
-            self.host   = cfg.get(section, "host")
+            self.host = cfg.get(section, "host")
         else:
-            self.host   = args["-H"]
+            self.host = args["-H"]
         self.basefolder = cfg.get(section, "folder")
-        self.url        = cfg.get(section, "url")
-        self.www_group  = cfg.get(section, "www-group")
+        self.url = cfg.get(section, "url")
+        self.www_group = cfg.get(section, "www-group")
 
         self.ssh_args.append(self.host)
 
@@ -125,21 +130,25 @@ class RPushHandler(object):
             self.cmd_push()
 
     def extra_handling(self):
-        if not self.args["--push"] and "clean" in self.args[ "<file>" ]:
+        if not self.args["--push"] and "clean" in self.args["<file>"]:
             print("Please specify all or the filenumber you want to clean.")
-            print("If you want to push file \"clean\" please specify push.")
+            print('If you want to push file "clean" please specify push.')
             sys.exit(1)
 
     def cmd_push(self):
         for f_in, f_out in it.chain(
-                zip(self.args["<file>"], self.args["<file>"]),
-                zip(self.args["<file_in>"], self.args["<file_out>"])):
+            zip(self.args["<file>"], self.args["<file>"]),
+            zip(self.args["<file_in>"], self.args["<file_out>"]),
+        ):
             rfolder = random_string()
-            self.run_ssh_command("mkdir "+shell_quote(rfolder))
+            self.run_ssh_command("mkdir " + shell_quote(rfolder))
             path = osp.join(rfolder, os.path.basename(f_out))
             self.run_scp_command(f_in, path)
-            self.run_ssh_command("chown :{1} {0} && chmod g+r {0}".format(
-                shell_quote(path), self.www_group))
+            self.run_ssh_command(
+                "chown :{1} {0} && chmod g+r {0}".format(
+                    shell_quote(path), self.www_group
+                )
+            )
 
             print(self.encode_url(path))
 
@@ -156,15 +165,19 @@ class RPushHandler(object):
             if self.args["--url-only"]:
                 print("{0}/{1}".format(self.url, urllib.quote(f)))
             else:
-                print("[{0}]\t'{1}'\t{2}/{3}".format(
-                    i, f, self.url, urllib.quote(f)))
+                print(
+                    "[{0}]\t'{1}'\t{2}/{3}".format(
+                        i, f, self.url, urllib.quote(f)
+                    )
+                )
 
     def cmd_clean(self):
         complete_content = self.get_complete_remote_content()
 
-        if not self.args[ "all" ]:
-            to_delete = [complete_content[int(i)]
-                    for i in set(self.args["<num>"])]
+        if not self.args["all"]:
+            to_delete = [
+                complete_content[int(i)] for i in set(self.args["<num>"])
+            ]
         else:
             to_delete = complete_content
 
@@ -179,13 +192,20 @@ class RPushHandler(object):
 
     def run_ssh_command(self, command):
         logging.debug(command)
-        return self.ssh_exec(self.ssh_args
-            + ["cd {0} && {1}".format(shell_quote(self.basefolder), command)])
+        return self.ssh_exec(
+            self.ssh_args
+            + ["cd {0} && {1}".format(shell_quote(self.basefolder), command)]
+        )
 
     def run_scp_command(self, path_from, path_to):
-        logging.debug( "SCP: {0} -> {1}".format(path_from, path_to))
-        return self.ssh_exec(["scp", path_from, "{0}:'{1}/{2}'".format(
-            self.host, self.basefolder, path_to)])
+        logging.debug("SCP: {0} -> {1}".format(path_from, path_to))
+        return self.ssh_exec(
+            [
+                "scp",
+                path_from,
+                "{0}:'{1}/{2}'".format(self.host, self.basefolder, path_to),
+            ]
+        )
 
     def ssh_exec(self, cmd):
         logging.debug(" ".join(cmd))
@@ -220,5 +240,3 @@ if __name__ == "__main__":
         logging.root.setLevel(logging.DEBUG)
         #  logging.debug(arguments)
     RPushHandler(arguments)
-
-
